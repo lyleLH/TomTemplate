c3d2100cce9507185c798402fe8de4fb3d7676e7
MTTGCC <v2top1@163.com>
2021-03-06 15:46:54 +0800

    --

--- templates/ios/Example/Source/Common/Helper/Network/CPDNetWorking.h
+++ templates/ios/Example/Source/Common/Helper/Network/CPDNetWorking.h
@@ -8,43 +8,43 @@
 
 #import <Foundation/Foundation.h>
 #import "CPDAPIMethod.h"
-#import <YKNetWorking/YKNetWorking.h>
+// #import <YKNetWorking/YKNetWorking.h>
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CPDNetWorking : YKNetWorking
+// @interface CPDNetWorking : YKNetWorking
 
-#pragma mark -- 统一处理异常的方法 ,不会将Error返回给上层
-+ (YKURLSessionTask *)syt_postWithApiClass:(kCPDAPIClass)class
-                                  ApiMethod:(kCPDAPIMethod)method
-                                refreshCache:(BOOL)refreshCache
-                                      params:(NSDictionary *)params
-success:(YKResponseSuccess)success ;
+// #pragma mark -- 统一处理异常的方法 ,不会将Error返回给上层
+// + (YKURLSessionTask *)syt_postWithApiClass:(kCPDAPIClass)class
+//                                   ApiMethod:(kCPDAPIMethod)method
+//                                 refreshCache:(BOOL)refreshCache
+//                                       params:(NSDictionary *)params
+// success:(YKResponseSuccess)success ;
 
-+ (YKURLSessionTask *)syt_getWithApiClass:(kCPDAPIClass)class
-                                  ApiMethod:(kCPDAPIMethod)method
-                                refreshCache:(BOOL)refreshCache
-                                      params:(NSDictionary *)params
-success:(YKResponseSuccess)success ;
+// + (YKURLSessionTask *)syt_getWithApiClass:(kCPDAPIClass)class
+//                                   ApiMethod:(kCPDAPIMethod)method
+//                                 refreshCache:(BOOL)refreshCache
+//                                       params:(NSDictionary *)params
+// success:(YKResponseSuccess)success ;
  
 
-#pragma mark -- 单独获取异常
+// #pragma mark -- 单独获取异常
 
 
-+ (YKURLSessionTask *)syt_postWithApiClass:(kCPDAPIClass)class
-                                  ApiMethod:(kCPDAPIMethod)method
-                               refreshCache:(BOOL)refreshCache
-                                     params:(NSDictionary *)params
-                                    success:(YKResponseSuccess)success
-fail:(YKResponseFail)fail ;
+// + (YKURLSessionTask *)syt_postWithApiClass:(kCPDAPIClass)class
+//                                   ApiMethod:(kCPDAPIMethod)method
+//                                refreshCache:(BOOL)refreshCache
+//                                      params:(NSDictionary *)params
+//                                     success:(YKResponseSuccess)success
+// fail:(YKResponseFail)fail ;
 
 
-+ (YKURLSessionTask *)syt_getWithApiClass:(kCPDAPIClass)class
-                                  ApiMethod:(kCPDAPIMethod)method
-                               refreshCache:(BOOL)refreshCache
-                                     params:(NSDictionary *)params
-                                    success:(YKResponseSuccess)success
-fail:(YKResponseFail)fail;
+// + (YKURLSessionTask *)syt_getWithApiClass:(kCPDAPIClass)class
+//                                   ApiMethod:(kCPDAPIMethod)method
+//                                refreshCache:(BOOL)refreshCache
+//                                      params:(NSDictionary *)params
+//                                     success:(YKResponseSuccess)success
+// fail:(YKResponseFail)fail;
 
-@end
+// @end
 
 NS_ASSUME_NONNULL_END
